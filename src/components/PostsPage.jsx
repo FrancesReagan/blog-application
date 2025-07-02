e0346fbe5de5d40d82f2adaf1f0ee8cac6e56d46
Frances Reagan James <adtgwen@gmail.com>
2025-07-02 08:31:43 -0400

    added content to postspage

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -15,7 +15,20 @@ function PostsPage() {
     const fetchPosts = async () => {
       try {
         // make a request to DummyJSON API
-        const repsone = await fetc"https://dummyjson.com/posts");
+        const repsone = await fetc("https://dummyjson.com/posts");
       }
- 
+    )};
 
+    // making a requset to the DummyJson API//
+    const response = await fetch('http://dummyjson.com/posts');
+    // check if the request was successful//
+    if (!response.ok) {
+      throw new Error(`Failed to fetch posts`);
+    }
+
+    // convert response to JSON//
+    const data = await response.json();
+
+    // stores the posts in the state(take only first 10 for better display
+  setPosts(data.posts.slice(0,10));
+  setLoading(false);
