1939c72aa840c053d7049b18d4f46b8bb32f15eb
Frances Reagan James <adtgwen@gmail.com>
2025-07-02 10:53:57 -0400

    added post, loading, error states for function PostDetailPage and fetch post

---
+++ src/components/PostDetailPage.jsx
@@ -0,0 +1,57 @@
+// PostDetailPage//
+import React, { use } from "react";
+import { useState, useEffect } from "react";
+import { useParams,Link} from "react-router-dom";
+
+function PostDetailPage() {
+  // extract the postId from the URL (e.g. like/posts/222->postId="222")
+  const { postId } = useParams();
+
+  // state to store the individual post data//
+  const [post, setPost] = useState(null);
+
+  // state to track loading status//
+  const [loading, setLoading] = useState(true);
+
+  // state to handle errors//
+  const [error, setError] = useState("null");
+
+  // fetch a specific post when the component loads or postId changes//
+  useEffect(() => {
+    const fetchPost = async() => {
+      try {
+        // fetch a specific post using the postId from the URL//
+        const response = await fetch(`https://dummyjson.com/posts/${postId}`);
+
+        if(!response.ok) {
+          throw new Error("Post is not found");
+        }
+
+        const postData = await response.json();
+        setPost(postData);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+      };
+
+      fetchPost();
+      // re-run if postId changes//
+    },[postId];
+
+    // show loading state//
+    if (loading) {
+      return (
+        <div className="loading-container">
+          <h2 className="loading-title">📖 Loading post details...</h2>
+          <p className="loading-text">Fetching post #{postId} for you.</p>
+        </div>
+      );
+    }
+
+
+
+  })
+
+}
