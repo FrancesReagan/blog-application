e8b1aaa27fb8b66bd34318893236c9a0105eac00
Frances Reagan James <adtgwen@gmail.com>
2025-07-02 02:18:40 -0400

    added title to homepage

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HomePage() {
+return (
+  <div className="home-welcome">
+    {/* main heading for home page */}
+<h1 className="home-title">
+  🏠Welcome to the Blog website</h1>
+
+ {/* a friendlt greeting
+ */}
+
+  
+  </div>
+)
+
+}
