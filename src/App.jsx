d97c9e982acc0de111d910a08d57ff6ee9f1edd9
Frances Reagan James <adtgwen@gmail.com>
2025-07-01 23:17:54 -0400

    adding nav links to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,28 @@ import PostsPage from "./components/PostsPage";
 import PostDetailPage from "./components/PostDetailPage";
 
 // navigation component -this appears on every page//
+function Navigation() {
+// useLocation gives user info about the current URL//
+const location = useLocation();
+return (
+  <nav className="navigation">
+    <div className="nav-container">
+      {/* logo/brand */}
+      <Link to="/" className="nav-logo"> 
+      🌟My Blog App
+      </Link>
+
+      {/* navigation links */}
+      <div className="nav-links">
+        <Link
+         to="/"
+      </div>
+    </div>
+  </nav>
+)
+
+
+}
 
 function App() {
   const [count, setCount] = useState(0)
